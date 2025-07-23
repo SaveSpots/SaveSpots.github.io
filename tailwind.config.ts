56ab64106dd60afe6575e2d6c16f47d929d1bacb
V-Paritosh <paritoshv08@gmail.com>
2025-07-23 01:48:13 -0500

    added a new font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Helvetica", "Arial", "sans-serif"],
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
@@ -106,5 +109,5 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
 export default config
